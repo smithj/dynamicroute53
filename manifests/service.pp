4015fee83946935054bd7d6d4d85dbd4644ab77c
Alan Jenkins <alan.jenkins@claranet.uk>
2017-05-09 12:39:59 +0100

    Make this work with systemd.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,10 @@
 class dynamicroute53::service {
+  if ! defined Exec['systemd-daemon-reload'] {
+    exec {'systemd-daemon-reload':
+      command     => 'systemctl daemon-reload'
+      refreshonly => true,
+    }
+  }
 
   file { '/usr/bin/update-dns-route53':
     ensure  => present,
@@ -18,9 +24,9 @@ class dynamicroute53::service {
     notify  => Service['updatednsroute53']
   }
 
-  file { '/etc/init.d/updatednsroute53':
+  file { '/etc/systemd/system/updatednsroute53.service':
     ensure  => present,
-    source  => 'puppet:///modules/dynamicroute53/updatednsroute53',
+    source  => 'puppet:///modules/dynamicroute53/updatednsroute53.service',
     owner   => root,
     group   => root,
     mode    => '754',
@@ -28,12 +34,15 @@ class dynamicroute53::service {
       File['/usr/bin/update-dns-route53'],
       File['/usr/bin/delete-dns-route53']
     ],
-    notify  => Service['updatednsroute53']
+    notify  => [
+      Service['updatednsroute53'],
+      Exec['systemd-daemon-reload'],
+    ]
   }
 
   service { 'updatednsroute53':
     ensure  => running,
-    require => File['/etc/init.d/updatednsroute53']
+    require => File['/etc/systemd/system/updatednsroute53.service']
   }
 
 }
